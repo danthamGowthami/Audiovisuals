525759c490973a491b482249853a22ffea8a0c37
danthamGowthami <76723802+danthamGowthami@users.noreply.github.com>
2021-10-09 16:06:15 +0530

    Add files via upload

---
+++ artfunctions.js
@@ -0,0 +1,42 @@
+function Spb()
+{
+document.getElementById("spb").innerHTML="<big>Best Of Balasubramanyam 2020</big>"+"<br>"+"1.Dhummu Dhooli"+"<br>"+"2.Emo Emo Ye Gundello"+"<br>"+"3.hivam Shivam"+"<br>"+"4.Vekuvamma Vekuvamma"+"<br>"+"5.Vedha Vaasin"+"<br>"+"6. Saho Shanmukha"+"<br>"+"7.Nuvve Kanta Padavante"+"<br>"+"8.Mounam Manasullona "+"<br>"+"9.Kurralloy Kurrallu"+"<br>"+"And many more</ul>";
+ }
+function Dsp()
+{
+document.getElementById("dsp").innerHTML="<big>List of Top 10 Devi Sri Prasad Songs of All Time</big>"+"<br>"+"<u>"+"<li>Mr. Perfect</li>"+"<li>Ammadu Lets Do Kummudu</li>"+"<li>Yentha Sakkagunnave</li>"+"<li>My Love Is Gone</li>"+"<li> Karige Loga</li>"+"<li>Ringa Ringa</li>"+"<li> Daddy Mummy</li>"+"<li>Eswara</li>"+"<li>Crazy Feeling</li>"+"<li>Crazy Feeling</li>"+
+"<ul>";
+}
+
+function Chandhan()
+{
+document.getElementById("Chandhan").innerHTML="<big>List of Top 10 Chandan Shetty Songs of All Time</big>"+"<u>"+"<li>Chocolate Girl</li>"+"<li> Appuge</li>"+"<li> Salige</li>"+"<li>Nodu Shiva</li>"+"<li>Baaro Pailwaan</li>"+"<li> Party Freak</li>"+"<li>Kolumande</li>"+"<li>Pogaru Title Track</li>"+"<li>3 Peg</li>"+"<li>Karabuu</li>"+
+"<ul>";
+}
+
+function Shreya()
+{
+document.getElementById("Shreya").innerHTML="<big>Best of Shreya Ghoshal Songs</big>"+"<ul>"+"<li>Sun Raha Hai Na Tu</li>"+"<li>Nagada Sang Dhol</li>"+"<li>Dola Re Dola</li>"+"<li>Kabhi Jo Baadal Barse</li>"+"<li>Yeh Ishq Hai</li>"+"<li>Saans</li>"+"<li>Teri Meri</li>"+"<li> Bahara</li>"+"<li> Jaadu Hai Nasha hai (lyrics)</li>"+"<li> Manwa Laage</li>"+
+"</ul>";
+}
+
+function Hema()
+{
+document.getElementById("Hema").innerHTML="<big>Singer Hemachandra Best Songs in Telugu</big>"+"<ul>"+"<li>Naalo Nenena</li>"+"<li>Extraordinary</li>"+"<li>Premadesam Yuva Rani</li>"+"<li>Alae Baale</li>"+"<li>Piliche Pedavalu Paina</li>"+
+"</ul>";
+}
+
+
+function Sid()
+{
+document.getElementById("Sid").innerHTML="<big>Best of SidSriram</big>"+"<ul>"+"<li>Uchanthala Regaiyile</li>"+"<li>Idhuvum Kadandhu Pogum (The Healing Song)</li>"+"<li>Yaar Azhaippadhu</li>"+"<li>Nira</li>"+"<li>Kaalai Adhikaalai</li>"+"<li>Yaen Ennai Pirindhaai</li>"+"<li>Mudhal Nee Mudivum Nee Title Track</li>"+"<li>Edho Solla</li>"+"<li>Kaadhal Theevey</li>"+
+"</ul>";
+}
+
+function Revanth()
+{
+document.getElementById("Revanth").innerHTML="<big>List of Top 10 Revanth Songs of All Time</big>"+"<ul>"+"<li>Oopiri Aaguthunnadey</li>"+"<li>Chiranjeeva</li>"+"<li>Luckkanna Mate Nillu</li>"+"<li>Rap Up 2020</li>"+"<li> I Wanna Fly</li>"+"<li>Manohari</li>"
++"</ul>";
+}
+
+
